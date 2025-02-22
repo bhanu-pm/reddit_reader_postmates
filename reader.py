7e3c7449d9ffd0c2dc705afecd64ec814eb2eca2
bhanu-pm <bhanuprakashm1108@gmail.com>
2025-02-22 14:19:00 -0700

    Complete reader.py

--- reader.py
+++ reader.py
@@ -19,28 +19,30 @@ class Reader:
 		self.reddit = Reddit(client_id=Reader.client_id, client_secret=Reader.client_secret, user_agent=Reader.user_agent)
 		self.subreddit_name = subreddit_name
 		try:
-			with open('comments.json', 'r') as file:
+			with open('final_comments.json', 'r') as file:
 				self.comments_list_json = json.load(file)
 
 		except Exception as e:
 			self.comments_list_json = []
 			print(f"Error occured: {e}")
 
-	def fetch_comment_tree(self):
+	def fetch_unseen_comments(self):
 		# Stickied submission (Pinned post) obj
 		subreddit_obj = self.reddit.subreddit(self.subreddit_name)
 		submission_obj = subreddit_obj.sticky()
 		submission_obj.comment_sort = "new"
 		submission_obj.comments.replace_more(limit=None)
 		
+		unseen_comments_list = []
+		unseen_comments_dict = {}
 		for comment in submission_obj.comments:
-			print(f"Default: {comment}")
-			print(f"ID: {comment.id}")
-			print(f"Body: {comment.body}")
-			print(" ################################################### ")
+			if comment.id == self.comments_list_json[0]["id"]:
+				break
+			unseen_comments_dict["id"] = comment.id
+			unseen_comments_dict["body"] = comment.body
+			unseen_comments_list.append(unseen_comments_dict)
 
-	def get_latest_unseen_comment(self):
-		pass
+		return unseen_comments_list
 
 	def add_comment_to_dict(self, comment, location: str, code: str):
 		self.latest_comment_dict = {}
@@ -49,6 +51,11 @@ class Reader:
 		self.latest_comment_dict["location"] = location
 		self.latest_comment_dict["code"] = code
 		self.comments_list_json.insert(0, self.latest_comment_dict)
+
+	def dump_to_json(self, file_name:str = "final_comments.json"):
+		with open(file_name, "w") as file:
+			json.dump(self.comments_list_json, file, indent=4)
+
 
 if __name__ == "__main__":
 	read_subreddit = Reader("postmates")
