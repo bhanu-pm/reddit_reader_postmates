c188628a8e3bbacfd1ac8cfe065fc3d0715a90b5
bhanu-pm <bhanuprakashm1108@gmail.com>
2025-02-22 13:23:40 -0700

    Loading json, fetching top level sorted comments

--- reader.py
+++ reader.py
@@ -18,17 +18,26 @@ class Reader:
 	def __init__(self, subreddit_name="postmates"):
 		self.reddit = Reddit(client_id=Reader.client_id, client_secret=Reader.client_secret, user_agent=Reader.user_agent)
 		self.subreddit_name = subreddit_name
+		try:
+			with open('codes.json', 'r') as file:
+				self.comment_dict = json.load(file)
+
+		except Exception as e:
+			self.comment_dict = {}
+			print(f"Error occured: {e}")
 
 	def fetch_comment_tree(self):
 		# Stickied submission (Pinned post) obj
 		subreddit_obj = self.reddit.subreddit(self.subreddit_name)
 		submission_obj = subreddit_obj.sticky()
-
-		top_level_comments = submission_obj.comments
-
-		for comment in top_level_comments:
+		submission_obj.comment_sort = "new"
+		submission_obj.comments.replace_more(limit=None)
+		
+		for comment in submission_obj.comments:
 			print(comment)
-			print(type(comment))
+
+	def get_latest_unseen_comment(self):
+		pass
 
 
 if __name__ == "__main__":
